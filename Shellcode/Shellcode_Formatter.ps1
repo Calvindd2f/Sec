cf6d3dcc08cfd0af6037815f1dd01b8b46e8b525
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-02-18 22:51:42 +0000

    Update Shellcode_Formatter.ps1
    Pulled from private repo.
    Completely changed the  formatting.

--- Shellcode/Shellcode_Formatter.ps1
+++ Shellcode/Shellcode_Formatter.ps1
@@ -1,19 +1,34 @@
-$fileName = "C:\Users\User\Desktop\payload.bin"
+<#.
+
+Powershell shellcode formatter. Debating turning this into function but it works as is for now.
+Taken from a private repo I own - DEC 12 2022
+
+.#>
+
+$fileName = 'C:\Users\User\Desktop\payload.bin'
 $fileContent = [IO.File]::ReadAllBytes($fileName)
 $filecontentsencoded = [convert]::ToBase64String($fileContent)
-"Binary Blob base64 encoded:`n`n" + $filecontentsencoded | set-content ($fileName + ".b64")
+"Binary Blob base64 encoded:`n`n" + $filecontentsencoded | Set-Content ($fileName + '.b64')
 
-$scformat = '\x' + (($fileContent | ForEach-Object ToString x2) -join '\x')
-"`nStandard shellcode format:`n`n" + $scformat | add-content ($fileName + ".b64")
+$scformat = '\x' + (($fileContent | ForEach-Object -MemberName ToString x2) -join '\x')
+"`nStandard shellcode format:`n`n" + $scformat | Add-Content ($fileName + '.b64')
 
-$csharpformat = '0x' + (($fileContent | ForEach-Object ToString x2 | ForEach-Object { $_ + ',' }) -join '0x')
+$csharpformat = '0x' + (($fileContent |
+    ForEach-Object -MemberName ToString x2 |
+    ForEach-Object -Process {
+      $_ + ',' 
+}) -join '0x')
 $csharpformat = $csharpformat.SubString(0, $csharpformat.Length-1)
-"`nC# formatted shellcode:`n`n" + $csharpformat | add-content ($fileName + ".b64")
+"`nC# formatted shellcode:`n`n" + $csharpformat | Add-Content ($fileName + '.b64')
 
 $Bytes = [System.Text.Encoding]::UTF8.GetBytes($csharpformat)
-$EncodedText =[Convert]::ToBase64String($Bytes)
-"`nBase64 Encoded C# shellcode:`n`n" + $EncodedText | add-content ($fileName + ".b64")
+$EncodedText = [Convert]::ToBase64String($Bytes)
+"`nBase64 Encoded C# shellcode:`n`n" + $EncodedText | Add-Content ($fileName + '.b64')
 
-$fsharpformat = '0x' + (($fileContent | ForEach-Object ToString x2 | ForEach-Object { $_ + 'uy;' }) -join '0x')
+$fsharpformat = '0x' + (($fileContent |
+    ForEach-Object -MemberName ToString x2 |
+    ForEach-Object -Process {
+      $_ + 'uy;' 
+}) -join '0x')
 $fsharpformat = $fsharpformat.SubString(0, $fsharpformat.Length-1)
-"`nF# formatted shellcode:`n`n" + $fsharpformat | add-content ($fileName + ".b64")
+"`nF# formatted shellcode:`n`n" + $fsharpformat | Add-Content ($fileName + '.b64')
