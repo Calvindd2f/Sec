0d8d33673ce080f551f6f145d66c8e7aac881e77
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-02-18 23:18:53 +0000

    Update Download-Extract.ps1

--- Utility/Download-Extract.ps1
+++ Utility/Download-Extract.ps1
@@ -1,6 +1,6 @@
 $clipath = $args[0]
-$BackUpPath = [System.IO.Path]::Combine($args[1], "dotnet-latest.zip")
-$Destination = [System.IO.Path]::Combine($args[1], "dotnet")
+$BackUpPath = [System.IO.Path]::Combine($args[1], "Great.zip")
+$Destination = [System.IO.Path]::Combine($args[1], "Great_Directory")
 
 Invoke-WebRequest -UseBasicParsing $clipath -OutFile $BackUpPath
 Add-Type -assembly "system.io.compression.filesystem"
